8d5a9ca1e2d056d70bb44eeca3d899e95cfbf6e3
rxskee <rxskee@gmail.com>
2023-12-27 17:10:35 +0300

    Рефакторинг. Финальная версия.

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 from requests import get
 
 from WeatherSite.settings import WEATHER_API_KEY
 
 
-# Create your views here.
 def index(request):
     return render(request, 'main_app/index.html')
 
 
-def city(request):
+def city(request: HttpRequest):
     # Параметры запроса
     params = {
-        'q': request.GET.get('city'),
+        'q': request.GET.get(key='city', default='Москва'),
         'lang': 'ru',
         'units': 'metric',
         'appid': WEATHER_API_KEY,
     }
 
     # API запрос
-    weather_data = get(
-        'http://api.openweathermap.org/data/2.5/forecast?',
-        params=params).json()['list'][0]
+    try:
+        weather_data = get(
+            'http://api.openweathermap.org/data/2.5/forecast?',
+            params=params).json()['list'][0]
+    except KeyError:
+        err_context = {
+            'error': 'Ошибка при получении данных'
+        }
+        return render(request, template_name='main_app/city.html', context=err_context)
 
     # Определение направления ветра
     deg = weather_data['wind']['deg']
@@ -30,6 +36,7 @@ def city(request):
 
     # Контент
     context = {
+        'city': (request.GET.get(key='city', default='Москва')).capitalize(),
         'weather_type': weather_data['weather'][0]['description'],
         'main': weather_data['main'],
         'wind_speed': weather_data['wind']['speed'],
